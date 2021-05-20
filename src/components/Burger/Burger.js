d16f2e5dea84af039c6f311fb4260368c520246f
cs-mamun <mukhula.mamun.cse@ulab.edu.bd>
2021-05-21 03:14:22 +0600

    Burger control added next is make dynamic data set

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Classes from './Burger.module.css';
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+    let transformIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])]
+        .map((_,i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey}/>
+        })
+        .reduce((arr,el) => {
+            return arr.concat(el);
+        },[]);
+    });
+    if(transformIngredients.length === 0) {
+        transformIngredients = <p>Please start adding ingredients!</p>
+    }
+    console.log(transformIngredients);
+    return (
+      <div className={Classes.Burger}>
+        <BurgerIngredient type="bread-top" />
+        {transformIngredients}
+        <BurgerIngredient type="bread-bottom" />
+      </div>
+    );
+}
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Burger from "../../components/Burger/Burger";
 import Auxi from "../../hoc/Auxi";
-
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      cheese: 0,
+      bacon: 0,
+      meat: 0,
+    },
+  };
   render() {
     return (
       <Auxi>
-        <div>Burger</div>
-        <div>Burger content</div>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls/>
       </Auxi>
     );
   }

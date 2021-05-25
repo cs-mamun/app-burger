676b6c886cd5acb262d45eae0975e1415c511fc5
cs-mamun <mukhula.mamun.cse@ulab.edu.bd>
2021-05-26 01:39:20 +0600

    Responsive done next is Humburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import Auxi from '../../hoc/Auxi';
-import StyleClasses from './Layout.module.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import React, { Component } from "react";
+import Auxi from "../../hoc/Auxi";
+import StyleClasses from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
+class Layout extends Component {
+  state = {
+    sideDrawer: true,
+  };
+  sideDrawerCloseHandler = () => {
+    this.setState({ sideDrawer: false });
+  };
+  render() {
+    return (
+      <Auxi>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.sideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={StyleClasses.content}>{this.props.children}</main>
+      </Auxi>
+    );
+  }
+}
 
-const layout = (props) => (
-  <Auxi>
-    <Toolbar />
-    <main className={StyleClasses.content}>{props.children}</main>
-  </Auxi>
-);
-
-export default layout;
+export default Layout;
